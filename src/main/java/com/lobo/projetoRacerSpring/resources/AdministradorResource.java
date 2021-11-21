6c865663f99ba482b042f09038d6fd1f5a9fd3d8
EduardoLob <eduardohenriquelobo11@gmail.com>
2021-11-21 15:24:44 -0300

    Autorização usando token JWT

--- src/main/java/com/lobo/projetoRacerSpring/resources/AdministradorResource.java
+++ src/main/java/com/lobo/projetoRacerSpring/resources/AdministradorResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +42,8 @@ public class AdministradorResource {
 		List<AdministradorDTO> listDTO = list.stream().map(obj -> new AdministradorDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+	  
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<AdministradorDTO> create(@Valid @RequestBody AdministradorDTO objDTO){
 		Administrador newObj = service.create(objDTO);
@@ -49,12 +51,14 @@ public class AdministradorResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<AdministradorDTO> update(@PathVariable Integer id, @Valid @RequestBody AdministradorDTO objDTO){
 		Administrador obj = service.update(id, objDTO);
 		return ResponseEntity.ok().body(new AdministradorDTO(obj));
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<AdministradorDTO> delete(@PathVariable Integer id){
 		service.delete(id);
