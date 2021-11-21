6865d212e034b743d92f2a97b33af259e9dd7b7d
EduardoLob <eduardohenriquelobo11@gmail.com>
2021-11-21 10:55:59 -0300

    CRUD para o Usuario

---
+++ src/main/java/com/lobo/projetoRacerSpring/domain/dtos/UsuarioDTO.java
@@ -0,0 +1,105 @@
+package com.lobo.projetoRacerSpring.domain.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lobo.projetoRacerSpring.domain.Usuario;
+import com.lobo.projetoRacerSpring.domain.enums.Perfil;
+
+public class UsuarioDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	protected Integer id;
+	@NotNull(message = "Nome é obrigatório")
+	protected String nome;
+	@NotNull(message = "CPF é obrigatório")
+	protected String cpf;
+	@NotNull(message = "Email é obrigatório")
+	protected String email;
+	@NotNull(message = "Senha é obrigatório")
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
+
+	public UsuarioDTO() {
+		super();
+		addPerfil(Perfil.USUARIO);
+	}
+
+	public UsuarioDTO(Usuario obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+		addPerfil(Perfil.USUARIO);  
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfil(Perfil perfil) {
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+}
---
+++ src/main/java/com/lobo/projetoRacerSpring/services/UsuarioService.java
@@ -0,0 +1,67 @@
+package com.lobo.projetoRacerSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lobo.projetoRacerSpring.domain.Usuario;
+import com.lobo.projetoRacerSpring.domain.Pessoa;
+import com.lobo.projetoRacerSpring.domain.dtos.UsuarioDTO;
+import com.lobo.projetoRacerSpring.repositories.UsuarioRepository;
+import com.lobo.projetoRacerSpring.repositories.PessoaRepository;
+import com.lobo.projetoRacerSpring.services.exceptions.DataIntegrityViolationException;
+import com.lobo.projetoRacerSpring.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Usuario findById(Integer id) {
+		Optional<Usuario> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Não encontramos o usuario de ID: " + id));
+	}
+
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+
+	public Usuario create(UsuarioDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Usuario newObj = new Usuario(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Usuario update(Integer id, @Valid UsuarioDTO objDTO) {
+		objDTO.setId(id);
+		Usuario oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Usuario(objDTO);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Usuario obj = findById(id);
+		repository.deleteById(id); 
+	}
+
+	private void validaPorCpfEEmail(UsuarioDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já está cadastrado em nosso sistema");
+		}
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já está cadastrado em nosso sistema");
+		}
+	}
+
+}
